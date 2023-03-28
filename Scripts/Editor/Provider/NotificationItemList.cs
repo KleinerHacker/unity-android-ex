25d63ee2ec273ea0d72a56977297836457580fed
KleinerHacker <Christoph.Pfeiffer.Berlin@freenet.de>
2023-03-28 22:12:50 +0200

    insert notification system

---
+++ Scripts/Editor/Provider/NotificationItemList.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using UnityAndroidEx.Runtime.android_ex.Scripts.Runtime.Assets;
+using UnityCommonEx.Runtime.common_ex.Scripts.Runtime.Utils.Extensions;
+using UnityEditor;
+using UnityEditorEx.Editor.editor_ex.Scripts.Editor;
+using UnityEngine;
+
+namespace UnityAndroidEx.Editor.android_ex.Scripts.Editor.Provider
+{
+    public sealed class NotificationItemList : TableReorderableList
+    {
+        public NotificationItemList(SerializedObject serializedObject, SerializedProperty elements) : base(serializedObject, elements)
+        {
+            Columns.Add(new FixedColumn { HeaderText = "Channel", AbsoluteWidth = 150f, ElementCallback = OnChannel });
+            Columns.Add(new FixedColumn { HeaderText = "Title", AbsoluteWidth = 300f, ElementCallback = OnTitle });
+            Columns.Add(new FlexibleColumn { HeaderText = "Description", ElementCallback = OnMessage });
+            Columns.Add(new FixedColumn { HeaderText = "Delay in minutes", AbsoluteWidth = 100f, MaxHeight = 20f, ElementCallback = OnDelay });
+
+            elementHeight = 85f;
+        }
+
+        private void OnChannel(Rect rect, int i, bool isactive, bool isfocused)
+        {
+            var prop = serializedProperty.GetArrayElementAtIndex(i).FindPropertyRelative("channelRef");
+            var channels = NotificationSettings.Singleton.Channels.Select(x => x.ID).ToArray();
+            var index = channels.IndexOf(x => string.Equals(x, prop.stringValue));
+            var newIndex = EditorGUI.Popup(rect, index, channels);
+            if (newIndex != index)
+            {
+                prop.stringValue = newIndex < 0 ? "" : channels[newIndex];
+            }
+        }
+
+        private void OnTitle(Rect rect, int i, bool isactive, bool isfocused)
+        {
+            var prop = serializedProperty.GetArrayElementAtIndex(i).FindPropertyRelative("title");
+            EditorGUI.PropertyField(rect, prop, GUIContent.none);
+        }
+
+        private void OnMessage(Rect rect, int i, bool isactive, bool isfocused)
+        {
+            var prop = serializedProperty.GetArrayElementAtIndex(i).FindPropertyRelative("text");
+            EditorGUI.PropertyField(rect, prop, GUIContent.none);
+        }
+
+        private void OnDelay(Rect rect, int i, bool isactive, bool isfocused)
+        {
+            var prop = serializedProperty.GetArrayElementAtIndex(i).FindPropertyRelative("minutesDelayed");
+            EditorGUI.PropertyField(rect, prop, GUIContent.none);
+        }
+    }
+
+    public sealed class NotificationChannelList : TableReorderableList
+    {
+        public NotificationChannelList(SerializedObject serializedObject, SerializedProperty elements) : base(serializedObject, elements)
+        {
+            Columns.Add(new FixedColumn { HeaderText = "ID", AbsoluteWidth = 100f, MaxHeight = 20f, ElementCallback = OnID });
+            Columns.Add(new FixedColumn { HeaderText = "Name", AbsoluteWidth = 300f, ElementCallback = OnName });
+            Columns.Add(new FlexibleColumn { HeaderText = "Description", ElementCallback = OnDescription });
+            Columns.Add(new FixedColumn { HeaderText = "Importance", AbsoluteWidth = 100f, ElementCallback = OnImportance });
+
+            elementHeight = 85f;
+        }
+
+        private void OnID(Rect rect, int i, bool isactive, bool isfocused)
+        {
+            var prop = serializedProperty.GetArrayElementAtIndex(i).FindPropertyRelative("id");
+            EditorGUI.PropertyField(rect, prop, GUIContent.none);
+        }
+
+        private void OnName(Rect rect, int i, bool isactive, bool isfocused)
+        {
+            var prop = serializedProperty.GetArrayElementAtIndex(i).FindPropertyRelative("name");
+            EditorGUI.PropertyField(rect, prop, GUIContent.none);
+        }
+
+        private void OnDescription(Rect rect, int i, bool isactive, bool isfocused)
+        {
+            var prop = serializedProperty.GetArrayElementAtIndex(i).FindPropertyRelative("description");
+            EditorGUI.PropertyField(rect, prop, GUIContent.none);
+        }
+
+        private void OnImportance(Rect rect, int i, bool isactive, bool isfocused)
+        {
+            var prop = serializedProperty.GetArrayElementAtIndex(i).FindPropertyRelative("importance");
+            EditorGUI.PropertyField(rect, prop, GUIContent.none);
+        }
+    }
+}
---
+++ Scripts/Runtime/UnityAndroidExStartupEvents.cs
@@ -0,0 +1,51 @@
+﻿#if PCSOFT_ANDROID_NOTIFICATION && PLATFORM_ANDROID
+using Unity.Notifications.Android;
+using UnityAndroidEx.Runtime.android_ex.Scripts.Runtime.Assets;
+using UnityAssetLoader.Runtime.asset_loader.Scripts.Runtime;
+using UnityEngine;
+
+namespace UnityAndroidEx.Runtime.android_ex.Scripts.Runtime
+{
+    //Permission android.permission.POST_NOTIFICATIONS required
+    public static class UnityAndroidExStartupEvents
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+        public static void Init()
+        {
+            Debug.Log("[ANDROID-NOTIFY] Initialize notification...");
+            AssetResourcesLoader.LoadFromResources<NotificationSettings>("");
+
+#if PCSOFT_ANDROID_NOTIFICATION_LOGGING
+            Debug.Log("[ANDROID-NOTIFY] Register channels... (" + NotificationSettings.Singleton.Channels.Length + ")");
+#endif
+            foreach (var channel in NotificationSettings.Singleton.Channels)
+            {
+#if PCSOFT_ANDROID_NOTIFICATION_LOGGING
+                Debug.Log("[ANDROID-NOTIFY] Register channels " + channel.ID + " (" + channel.Name + ")");
+#endif
+                var androidChannel = new AndroidNotificationChannel(channel.ID, channel.Name, channel.Description, channel.Importance);
+                AndroidNotificationCenter.RegisterNotificationChannel(androidChannel);
+            }
+
+#if PCSOFT_ANDROID_NOTIFICATION_LOGGING
+            Debug.Log("[ANDROID-NOTIFY] Register notifications... (" + NotificationSettings.Singleton.Items.Length + ")");
+#endif
+            AndroidNotificationCenter.CancelAllNotifications();
+            foreach (var item in NotificationSettings.Singleton.Items)
+            {
+#if PCSOFT_ANDROID_NOTIFICATION_LOGGING
+                Debug.Log("[ANDROID-NOTIFY] Register notification " + item.Title + " on channel " + item.ChannelRef);
+#endif
+                var notification = new AndroidNotification
+                {
+                    Title = item.Title,
+                    Text = item.Text,
+                    FireTime = System.DateTime.Now.AddMinutes(item.MinutesDelayed)
+                };
+
+                AndroidNotificationCenter.SendNotification(notification, item.ChannelRef);
+            }
+        }
+    }
+}
+#endif
